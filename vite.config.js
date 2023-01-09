f74695358741355fc41f8a9af86d9bbf0d87d7b6
bcoalova <coalovaboris@gmail.com>
2023-01-09 14:26:21 -0300

    added responsive font

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/vacaciones',
+    base: '/vacaciones23',
     // build: {
     // base: '/vacaciones23/'
     // },
